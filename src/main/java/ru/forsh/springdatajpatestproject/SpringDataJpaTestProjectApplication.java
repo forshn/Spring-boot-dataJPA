e6ca65ff23172ee92b32916a6a39a5237558315f
Nikolay <forsh_nikolay@mail.ru>
2022-04-11 15:49:39 +0300

    Spring Data JPA project init

---
+++ src/main/java/ru/forsh/springdatajpatestproject/SpringDataJpaTestProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.forsh.springdatajpatestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaTestProjectApplication.class, args);
+	}
+
+}
